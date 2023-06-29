dd142b0835d8b67de7574f5ad7d1ffd009e99d11
LinkShake <nicolacampo19@gmail.com>
2023-06-29 16:56:04 +0200

    English support for chatGPT response

--- api/askChatGPT/src/index.ts
+++ api/askChatGPT/src/index.ts
@@ -5,6 +5,7 @@ import cors from "@fastify/cors";
 import { Configuration } from "openai";
 import fetch from "node-fetch";
 import { requestGPTCompletion } from "../../helpers";
+import LanguageDetect from "languagedetect";
 
 async function start() {
   const app = fastify();
@@ -22,15 +23,23 @@ async function start() {
     try {
       //@ts-ignore
       const { query } = req.params;
+      const langDetector = new LanguageDetect();
+      const prob = langDetector.detect(query);
       const res = await fetch(`http://localhost:4000/cognitive/${query}`);
       const data = await res.json();
       const { data: parsedData }: { data: string[] } = data;
-      //if (!parsedData?.length) return "No data matching your query";
-      console.log(parsedData);
-      const newQuery = `${query}, sapendo che: ${parsedData.slice(0, 15)}`;
+      //possibility of having a response either in italian (if your query matches italian) or english in any other case
+      const newQuery = `${query}, ${
+        prob[0][0] === "italian" ? "sapendo che" : "knowing that"
+      }: ${parsedData.slice(0, 15)}`;
 
       let gptRes = await requestGPTCompletion(newQuery);
-      if (parsedData?.length) gptRes = `Secondo la documentazione: \n${gptRes}`;
+      if (parsedData?.length)
+        gptRes = `${
+          prob[0][0] === "italian"
+            ? "Secondo la documentazione"
+            : "The documentation says"
+        }: \n${gptRes}`;
       return JSON.stringify(gptRes);
     } catch (err) {
       return JSON.stringify({ err });
