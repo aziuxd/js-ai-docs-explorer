4fd09b1e0e7c8fe355c77f589896afdcd9104f5a
LinkShake <nicolacampo19@gmail.com>
2023-06-30 12:03:41 +0200

    Added linkified and highlighted messages in response

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,11 +3,61 @@ import "./styles.css";
 import { Textarea, Button } from "@mantine/core";
 import { IconSend } from "@tabler/icons-react";
 import { useRef, useState, useEffect } from "react";
-import { flushSync } from "react-dom";
+import Highlighter from "react-highlight-words";
+import Linkify from "linkify-react";
 
 interface CustomError {
   message?: string;
 }
+
+const findChunks = ({
+  // @ts-ignore
+  autoEscape,
+  // @ts-ignore
+  caseSensitive,
+  // @ts-ignore
+  sanitize,
+  // @ts-ignore
+  searchWords,
+  // @ts-ignore
+  textToHighlight,
+}) => {
+  // @ts-ignore
+  const chunks = [];
+  const textLow = textToHighlight.toLowerCase();
+  const sep = /[\s]+/;
+
+  const singleTextWords = textLow.split(sep);
+
+  let fromIndex = 0;
+  const singleTextWordsWithPos = singleTextWords.map((s: any) => {
+    const indexInWord = textLow.indexOf(s, fromIndex);
+    fromIndex = indexInWord;
+    return {
+      word: s,
+      index: indexInWord,
+    };
+  });
+
+  // @ts-ignore
+  searchWords.forEach((sw) => {
+    const swLow = sw.toLowerCase();
+    // @ts-ignore
+    singleTextWordsWithPos.forEach((s) => {
+      if (s.word.includes(swLow)) {
+        const start = s.index;
+        const end = s.index + s.word.length;
+        chunks.push({
+          start,
+          end,
+        });
+      }
+    });
+  });
+
+  // @ts-ignore
+  return chunks;
+};
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState<string>("");
@@ -145,7 +195,26 @@ const PrettifiedData = ({ data }: { data: string }) => {
   return (
     <>
       <h2>{data.slice(0, data.indexOf(":"))}</h2>
-      <p>{data.slice(data.indexOf(":") + 1)}</p>
+      <Highlighter
+        highlightClassName="YourHighlightClass"
+        textToHighlight={data.slice(data.indexOf(":") + 1)}
+        searchWords={["http", "@"]}
+        autoEscape={true}
+        //@ts-ignore
+        findChunks={findChunks}
+        highlightTag={({ children, highlightIndex }) => (
+          <Linkify>
+            <p
+              style={{
+                color: "#67e8f9",
+              }}
+            >
+              {children}
+            </p>
+          </Linkify>
+        )}
+      />
+      {/*<p>{data.slice(data.indexOf(":") + 1)}</p>*/}
     </>
   );
 };
