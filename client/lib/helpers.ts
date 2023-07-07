bf73b08a282d26f114a2c9e837b0bd55811770e8
LinkShake <nicolacampo19@gmail.com>
2023-07-07 10:19:15 +0200

    Removed UI bug

--- client/lib/helpers.ts
+++ client/lib/helpers.ts
@@ -1,20 +1,10 @@
-import { useUser } from "@clerk/nextjs";
-
 export const formatDataForUI = (dataToFormat: string) => {
   let formattedData = dataToFormat.slice(dataToFormat.indexOf(":") + 1);
   if (
-    !formattedData.includes("Secondo la documentazione") &&
-    !formattedData.includes("The documentation says")
+    !formattedData.includes("Secondo la documentazione:") &&
+    !formattedData.includes("The documentation says:")
   )
     return formattedData;
 
   return formattedData.slice(formattedData.indexOf(":") + 1);
-};
-
-export const useIndexes = async () => {
-  const { user } = useUser();
-  const res = await fetch(`http://localhost:4000/sql/${user?.id}`);
-  const data = await res.json();
-
-  return data;
 };
